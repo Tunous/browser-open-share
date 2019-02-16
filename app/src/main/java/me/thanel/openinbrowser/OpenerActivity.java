c53a6f60c83e1309c6c54fce5b7a1129a0a5671a
Łukasz Rutkowski <lukus178@gmail.com>
2019-02-16 12:32:02 +0100

    Allow opening non-url text as Google search

--- app/src/main/java/me/thanel/openinbrowser/OpenerActivity.java
+++ app/src/main/java/me/thanel/openinbrowser/OpenerActivity.java
@@ -4,13 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.URLUtil;
 import android.widget.Toast;
 
-import org.nibor.autolink.LinkExtractor;
-import org.nibor.autolink.LinkSpan;
-import org.nibor.autolink.LinkType;
-
-import java.util.EnumSet;
+import org.apache.commons.validator.routines.UrlValidator;
 
 public class OpenerActivity extends Activity {
 
@@ -26,27 +23,11 @@ public class OpenerActivity extends Activity {
             return;
         }
 
-        LinkExtractor linkExtractor = LinkExtractor.builder()
-                .linkTypes(EnumSet.of(LinkType.URL, LinkType.WWW))
-                .build();
         String extraText = intent.getStringExtra(Intent.EXTRA_TEXT);
-        Iterable<LinkSpan> extractedLinks = linkExtractor.extractLinks(extraText);
-        if (!extractedLinks.iterator().hasNext()) {
-            Toast.makeText(this, R.string.error_no_link, Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
+        Uri linkToOpen = LinkParser.parseText(extraText);
 
-        LinkSpan linkSpan = extractedLinks.iterator().next();
-        String linkToOpen = extraText.substring(linkSpan.getBeginIndex(), linkSpan.getEndIndex());
-
-        if (!linkToOpen.startsWith("http://") && !linkToOpen.startsWith("https://")) {
-            // Http scheme is required for activities to correctly handle links so we append it here
-            // if we found link without one.
-            linkToOpen = "http://" + linkToOpen;
-        }
         String defaultBrowserPackageName = BrowserUtils.getDefaultBrowserPackageName(this);
-        BrowserUtils.openWith(this, Uri.parse(linkToOpen), defaultBrowserPackageName);
+        BrowserUtils.openWith(this, linkToOpen, defaultBrowserPackageName);
 
         finish();
     }
