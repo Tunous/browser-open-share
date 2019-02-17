b256c3a5ee1416199097895ecd6e5be001663ced
Łukasz Rutkowski <lukus178@gmail.com>
2019-02-17 16:12:03 +0100

    Handle non-url text as Google search (#1)

---
+++ app/src/main/java/me/thanel/openinbrowser/LinkParser.java
@@ -0,0 +1,35 @@
+package me.thanel.openinbrowser;
+
+import android.net.Uri;
+
+import org.apache.commons.validator.routines.UrlValidator;
+
+class LinkParser {
+    private LinkParser() {
+    }
+
+    static Uri parseText(String text) {
+        String linkToValidate = textWithHttpScheme(text);
+
+        if (new UrlValidator().isValid(linkToValidate)) {
+            return Uri.parse(linkToValidate);
+        }
+        return buildSearchUri(text);
+    }
+
+    static Uri buildSearchUri(String searchTerm) {
+        return new Uri.Builder()
+                .scheme("https")
+                .authority("google.com")
+                .path("search")
+                .appendQueryParameter("q", searchTerm)
+                .build();
+    }
+
+    static String textWithHttpScheme(String text) {
+        if (!text.startsWith("http://") && !text.startsWith("https://")) {
+            return "http://" + text;
+        }
+        return text;
+    }
+}
