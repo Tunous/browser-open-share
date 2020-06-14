bcc8e502345f4258acbdf659db6cf69e425b232f
Łukasz Rutkowski <lukus178@gmail.com>
2020-06-14 21:06:52 +0200

    Extract links when shared content includes additional text (#2)

--- app/src/main/java/me/thanel/openinbrowser/LinkParser.java
+++ app/src/main/java/me/thanel/openinbrowser/LinkParser.java
@@ -3,18 +3,46 @@ package me.thanel.openinbrowser;
 import android.net.Uri;
 
 import org.apache.commons.validator.routines.UrlValidator;
+import org.nibor.autolink.LinkExtractor;
+import org.nibor.autolink.LinkSpan;
+import org.nibor.autolink.LinkType;
 
-class LinkParser {
+import java.util.EnumSet;
+import java.util.Iterator;
+
+final class LinkParser {
+
+    private static final UrlValidator urlValidator = new UrlValidator();
+    private static final LinkExtractor linkExtractor = LinkExtractor.builder()
+            .linkTypes(EnumSet.of(LinkType.URL, LinkType.WWW))
+            .build();
+
     private LinkParser() {
     }
 
     static Uri parseText(String text) {
-        String linkToValidate = textWithHttpScheme(text);
+        Uri extractedLink = extractLink(text);
+        if (extractedLink != null) {
+            return extractedLink;
+        }
 
-        if (new UrlValidator().isValid(linkToValidate)) {
+        String linkToValidate = textWithHttpScheme(text);
+        if (urlValidator.isValid(linkToValidate)) {
             return Uri.parse(linkToValidate);
         }
+
         return buildSearchUri(text);
+    }
+
+    private static Uri extractLink(String sourceText) {
+        Iterable<LinkSpan> extractedLinks = linkExtractor.extractLinks(sourceText);
+        Iterator<LinkSpan> iterator = extractedLinks.iterator();
+        if (!iterator.hasNext()) {
+            return null;
+        }
+        LinkSpan linkSpan = iterator.next();
+        String extractedLink = sourceText.substring(linkSpan.getBeginIndex(), linkSpan.getEndIndex());
+        return Uri.parse(textWithHttpScheme(extractedLink));
     }
 
     private static Uri buildSearchUri(String searchTerm) {
